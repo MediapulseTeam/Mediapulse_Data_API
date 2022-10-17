0f367f5a9f64ff183a1da7edc24a5382316e40c8
mpdatateam <112482533+mpdatateam@users.noreply.github.com>
2022-10-17 14:15:01 +0200

    Feature/add tableau api lib functionality (#6)
    * added the tableau library functionality
    
    * adapted the rest version to allow dowload of large time spans
    
    * set correct view

--- Online/Content/Traffic/Proprietary/python/configuration/config.py
+++ Online/Content/Traffic/Proprietary/python/configuration/config.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass
-
 from datetime import date
-
 from typing import List
 
 
@@ -9,6 +7,7 @@ from typing import List
 class ViewConfig:
     v_name: str
     v_id: str
+    date_chunks: bool
 
 
 @dataclass(frozen=True)
@@ -17,20 +16,22 @@ class WorkbookConfig:
     wb_id: str
     views: List[ViewConfig]
 
-
 START_DATE = date(year=2021, month=6, day=1)
 
 workbook_config = [
-
     WorkbookConfig(wb_name="ProprietaryAPITrafficDashboards",
                    wb_id="79119cc9-7a49-4117-9c3e-84b385e307be",
                    views=[ViewConfig(v_name="Brand Summary Data",
-                                     v_id='1ea0644b-15f7-4cf6-8a8d-f5be20de710d'),
+                                     v_id='1ea0644b-15f7-4cf6-8a8d-f5be20de710d',
+                                     date_chunks=False),
                           ViewConfig(v_name="Brand Daily Data",
-                                     v_id='67375f0e-d8b5-4ef0-a98a-b1e71d26beb7'),
+                                     v_id='67375f0e-d8b5-4ef0-a98a-b1e71d26beb7',
+                                     date_chunks=True),
                           ViewConfig(v_name="Asset and OS Summary Data",
-                                     v_id='197223c7-b106-4e3e-8812-55ee0749f6fd'),
+                                     v_id='197223c7-b106-4e3e-8812-55ee0749f6fd',
+                                     date_chunks=False),
                           ViewConfig(v_name="Asset and OS Daily Data",
-                                     v_id='a8c3c60d-9c05-403b-94a8-0b4c8df374ae')
+                                     v_id='a8c3c60d-9c05-403b-94a8-0b4c8df374ae',
+                                     date_chunks=True)
                           ])
 ]
